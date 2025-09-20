e5635347b967ec87389ad588520e84de0e99226a
Md-Shaon-Khan <shaon.iit52@gmail.com>
2025-09-20 13:59:49 +0600

    Added insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(arr):
+  
+    for i in range(1, len(arr)):
+        key = arr[i]     
+        j = i - 1
+        
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j] 
+            j -= 1
+
+        arr[j + 1] = key  
+
+
+my_list = [12, 11, 13, 5, 6]
+print("Original list:", my_list)
+
+insertion_sort(my_list)
+print("Sorted list:  ", my_list)
